8422033d61e34b7b39a0611274b4087d214ca64c
Nathan Carnes <nathan@nathancarnes.com>
2010-06-09 22:33:23 -0700

    Initial checkin

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Starkers
+ */
+
+get_header();
+?>
+<div id="main">
+	<h2>Error 404 - Not Found</h2>
+
+<?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
